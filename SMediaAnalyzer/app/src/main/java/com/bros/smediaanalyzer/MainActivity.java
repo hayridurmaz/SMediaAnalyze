60e6c6aa8fe953803241005cbf1579f7a626acf2
Batuhan <batuhanmertkarabulut@gmail.com>
2017-11-25 21:33:24 +0300

    Libs configuration

--- SMediaAnalyzer/app/src/main/java/com/bros/smediaanalyzer/MainActivity.java
+++ SMediaAnalyzer/app/src/main/java/com/bros/smediaanalyzer/MainActivity.java
@@ -1,20 +1,20 @@
 package com.bros.smediaanalyzer;
 
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.widget.TextView;
 
 import java.util.List;
 
+import twitter4j.*;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class MainActivity extends AppCompatActivity {
